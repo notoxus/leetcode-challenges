f801b8ed9cf39fa3530d5ec84067bce1d520c4e4
notoxus <summerthinh3@gmail.com>
2026-08-31 00:34:02 +0700

    Add my write up to that problem

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# This repo is where I solve my leetcode challenge to make my mind sharpen.
+# This repo is where I solve my leetcode challenge tio make my mind sharpen.
+
+## Medium
+
+### Problemset
+
+#### 53. Maximum Subarray
+[notebook](note/maximum_subarray.pdf)
+[solution](dsa/maximum_subarray)
--- dsa/maximum_subarray/maximum_sub_bf.cpp
+++ dsa/maximum_subarray/maximum_sub_bf.cpp
@@ -10,7 +10,7 @@ class Solution
     // Brute Force with Big(O) notation equals O(N^3)
     int maxSubArray(std::vector<int>& nums)
     {
-        int maxSum = INT_MIN;
+        int maxSum = INT_MIN; // -2^31
         for(int i = 0; i < nums.size(); i++){
             for(int j = i; j < nums.size(); j++){
                 int sum = 0;
@@ -23,43 +23,57 @@ class Solution
         return maxSum;
     }
     // Brute Force with Big(O) notation equals O(N^2)
-    void printSubarray(std::vector<int>& nums)
+    int maxSubArray1(std::vector<int>& nums) 
     {
-        std::vector<int> result;
-        int sum = INT_MIN;
+        int maxSum = INT_MIN; // you can also use int maxSum = nums[0]; instead
         for(int i = 0; i < nums.size(); i++){
-            int curSum = 0;
+            int sum = 0;
             for(int j = i; j < nums.size(); j++){
-                curSum += nums[j];
-                if(curSum > sum) {
-                    sum = curSum;
-                    result.clear();
-                    for(int k = i; k <= j; k++) result.push_back(nums[k]);
-                }
-            }
-        }
-        // std::cout << "The subarray [";
-        // for (int i = 0; i < result.size(); i++) {
-        //     std::cout << result[i];
-        //     if(i < result.size() - 1) std::cout << ", ";
-        // }
-
-        // std::cout << "] has the largest sum " << sum << std::endl;
-        std::cout << sum;
-    }
-    // Or
-    int maxSubArray1(std::vector<int>& nums) {
-        int maxSum = INT_MIN;
-        for(int i = 0; i < nums.size(); i++){
-            int currentSum = 0;
-            for(int j = i; j < nums.size(); j++){
-                currentSum += nums[j];
-                if(currentSum > maxSum) {
-                    maxSum = currentSum;
+                sum += nums[j];
+                if(sum > maxSum) {
+                    maxSum = sum;
                 }
             }
         }
         return maxSum;
+    }
+    // And if you want to print their result
+    void printSubarray(std::vector<int>& nums)
+    {
+        int maxSum = INT_MIN;
+        int startIndex = 0;
+        int endIndex = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    /* 
+                    Avoid copying the subarray every time maxSum changes like that:
+                    result.clear(); 
+                    for(int k = i; k <= j; k++) result.push_back(nums[k]);
+                    And reduce time complexity, store the start and end indices instead. 
+                    */
+                    startIndex = i;
+                    endIndex = j;
+                }
+            }
+        }
+
+        std::cout << "The subarray [";
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            std::cout << nums[i];
+
+            if (i < endIndex)
+                std::cout << ", ";
+        }
+
+        std::cout << "] has the largest sum " << maxSum << '\n';
     }
 };
 int main()
@@ -88,13 +102,15 @@ int main()
     const std::chrono::duration<double, std::milli> runtime =
         end - start;
     const std::chrono::duration<double, std::milli> runtime1 =
-        end - start;
+        end1 - start1;
 
     std::cout << result << '\n'
               << "Big(O) - O(N^3) runtime: " << std::fixed << std::setprecision(3)
               << runtime.count() << " ms\n"
               << result1 << '\n'
               << "Big(O) - O(N^2) runtime: " << runtime1.count() << " ms\n";
-
+    std::cout << sizeof(int) << '\n';
+    std::cout << INT_MIN << '\n';
+    std::cout << INT_MAX << '\n';
     return 0;
 }
